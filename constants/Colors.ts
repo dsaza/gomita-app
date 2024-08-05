24453022db1ac81a090446103ca34a6a936e8e1c
Dayron Daza <dayrondaza05@gmail.com>
2024-08-05 15:59:19 -0500

    refactor: Update primary color and add primaryForeground color in Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,6 +3,7 @@ export const COLORS = {
   black: '#333333',
   blackForeground: '#999999',
   primary: '#6A3EA1',
+  primaryForeground: '#7451A0',
   secondary: '#F8C715',
   secondaryForeground: '#F7D149',
   error: '#FF0000',

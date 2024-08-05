e67238ddd99a02f763332d511b2b4f89653e0a6d
Dayron Daza <dayrondaza05@gmail.com>
2024-08-05 18:11:48 -0500

    refactor: Handle PIN validation and API call in FormPin component

--- components/auth/form-pin.tsx
+++ components/auth/form-pin.tsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import { Pressable, Text, View } from "react-native";
 import { NumberKeyBoard } from "./number-keyboard";
 import { useLoginContext } from "@/hooks/use-login-context";
 import { COLORS } from "@/constants/colors";
+import { useClientContext } from "@/hooks/use-client-context";
 
 export function FormPin () {
-  const { pin, updateFormItem, changeLoading, updateStep } = useLoginContext();
+  const [error, setError] = useState<string | undefined>();
+  const { phone, pin, updateFormItem, changeLoading, updateStep } = useLoginContext();
+  const { callAPI } = useClientContext();
+
+  const handleFinish = (value: string) => {
+    setError(undefined);
+    changeLoading(true);
+
+    callAPI('/user/pre-login', {
+      method: 'POST',
+      body: JSON.stringify({ phone, pin: value })
+    })
+      .then((result) => {
+        if (result.isSuccess !== 200) {
+          updateFormItem('pin', '');
+          setError(result.data?.message ?? 'No se pudo iniciar sesión');
+          return;
+        }
+
+        updateStep('otp');
+      })
+      .finally(() => {
+        changeLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    console.log({error})
+  }, [error])
 
   return (
     <View
@@ -20,13 +50,11 @@ export function FormPin () {
         label="Por favor ingresa tu PIN para continuar."
         length={4}
         value={pin}
+        error={error}
         onUpdate={(value) => {
           updateFormItem('pin', value);
         }}
-        onFinished={(value) => {
-          changeLoading(true);
-          console.log('Sending pin...', value);
-        }}
+        onFinished={handleFinish}
       />
       <Pressable
         style={{

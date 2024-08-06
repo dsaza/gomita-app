f2f2157fb3bd9efadee40aeb290cc09354b44e7a
Dayron Daza <dayrondaza05@gmail.com>
2024-08-06 18:44:22 -0500

    feat: Add log out functionality to index page

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,5 +1,10 @@
-import { Text } from 'react-native';
+import { Pressable, Text } from 'react-native';
+import { useAuthContext } from '@/hooks/use-auth-context';
 
 export default function Index () {
-  return <Text>Index hello</Text>
+  const { logOut } = useAuthContext();
+
+  return <Pressable onPress={logOut}>
+    <Text>Log out</Text>
+  </Pressable>
 }
